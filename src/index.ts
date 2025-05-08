1624a5fd372b9fd81de40bb768f5b3c7df21ebde
Sajid Mahmud <mahmudSajid366@gmail.com>
2025-05-08 12:53:26 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+{
+  // Problem 1
+//   function formatString(input: string, toUpper?: boolean): string {
+//     if (toUpper === false) {
+//       return input.toLowerCase();
+//     } else {
+//       return input.toUpperCase();
+//     }
+//   }
+
+  //
+
+  // Problem 2
+
+// function filterByRating(
+//     items: { title: string; rating: number }[]
+// ): { title: string; rating: number }[] 
+// {
+//     return items.filter(item=> item.rating >= 4)
+// }
+
+//     const books= [
+//     { title: "Book A", rating: 4.5 },
+//     { title: "Book B", rating: 3.2 },
+//     { title: "Book C", rating: 5.0 }
+//     ];
+
+    
+
+  //
+
+
+    // Problem 3
+    // function concatenateArrays<T>(...arrays: T[][]): T[]{
+    //     return ([] as T[]).concat(...arrays)
+    // }
+    // 
+
+    // Problem 4
+    class Vehicle{
+        private  make : string;
+        private  year: number;
+    
+        constructor(make: string, year: number){
+            this.make = make;
+            this.year= year;
+            
+        } 
+        getInfo(){
+            return `Make: ${this.make}, Year: ${this.year}`;
+
+        }  
+        }
+    class Car extends Vehicle{
+        private model: string;
+        
+        constructor(make: string, year: number, model: string){
+            super(make, year)
+            this.model= model;
+        } 
+        getModel(){
+            return `Model: ${this.model}`
+        }
+        
+        }
+        const myCar = new Car('Toyota', 2020, 'Corolla')
+        console.log(myCar.getInfo())
+        console.log(myCar.getModel())
+    // 
+}
